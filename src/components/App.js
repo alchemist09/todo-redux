1cb11289289ee4bd4782fc8b6ba1f56f63df5553
Luke Olali <mugapedia@gmail.com>
2018-11-14 08:43:38 +0300

    Ties the components together within the main App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
 import '../css/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
 export default App;
