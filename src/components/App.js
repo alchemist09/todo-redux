89824b26297a8baf1d2aa33ce9d8c62ab80f6ea0
Luke Olali <mugapedia@gmail.com>
2018-11-15 17:39:33 +0300

    Fixes CSS issues import issues on the main App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
-import '../css/App.css';
+// import '../css/App.css';
 
 const App = () => (
   <div>
