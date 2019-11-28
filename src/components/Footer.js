9c3830dce458b130621e8cb0cb01c00cc5d794ac
Luke Olali <mugapedia@gmail.com>
2019-11-28 13:59:20 +0300

    Renames FilterLink component to FilterContainer to denote a connected component and match it with the exported component from 'containers/FilterLink.js thereby eliminating unnecessary confusion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import FilterLink from '../containers/FilterLink';
+import FilterContainer from '../containers/FilterLink';
 import { VisibilityFilters } from '../actions';
 
 const Footer = () => (
   <p>
-    Show <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
+    Show <FilterContainer filter={VisibilityFilters.SHOW_ALL}>All</FilterContainer>
     {', '}
-    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
+    <FilterContainer filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterContainer>
     {', '}
-    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
+    <FilterContainer filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterContainer>
   </p>
 )
 
